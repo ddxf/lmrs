ac518ae4771fdbe36f8ec1f5c31656ef603311b8
Line_tail <1424878857@qq.com>
2021-11-05 17:42:49 +0800

    git push

--- blog/resources/views/index/index.blade.php
+++ blog/resources/views/index/index.blade.php
@@ -2,6 +2,5 @@
 @section('content')
     <div class="container" style="height: 500px;text-align: center;">
         <h1 style="position: absolute;left: 35%;top: 30%;">继承模板的主页搞定了！</h1>
-        {{-- 这里是Blade注释 --}}
     </div>
 @endsection
