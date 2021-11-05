d9bec5725feb4c3b8749592f92982170bbb1bad0
Line_tail <1424878857@qq.com>
2021-11-05 19:32:59 +0800

    git push

--- blog/resources/views/layout/base.blade.php
+++ blog/resources/views/layout/base.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{URL::asset('sass/app.css')}}">
     <link rel="stylesheet" href="{{URL::asset('css/bootstrap.css')}}">
     <link rel="stylesheet" href="{{URL::asset('css/bootstrap-theme.css')}}">
 </head>
