7e86697fdcd3e04bf20e297c61ce64418495f4a0
Line_tail <1424878857@qq.com>
2022-03-28 14:15:29 +0800

    git push

--- blog/resources/views/layout/base.blade.php
+++ blog/resources/views/layout/base.blade.php
@@ -9,6 +9,17 @@
     <link rel="stylesheet" href="{{URL::asset('css/bootstrap-theme.css')}}">
     <script rel="stylesheet" href="{{URL::asset('css/jquery.min.js')}}"></script>
     <script rel="stylesheet" href="{{URL::asset('css/bootstrap.js')}}"></script>
+    <link href="https://fastly.jsdelivr.net/npm/@bootcss/v3.bootcss.com@1.0.35/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="https://fastly.jsdelivr.net/npm/@bootcss/v3.bootcss.com@1.0.35/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="https://fastly.jsdelivr.net/npm/@bootcss/v3.bootcss.com@1.0.35/examples/starter-template/starter-template.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="https://fastly.jsdelivr.net/npm/@bootcss/v3.bootcss.com@1.0.35/assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="https://fastly.jsdelivr.net/npm/@bootcss/v3.bootcss.com@1.0.35/assets/js/ie-emulation-modes-warning.js"></script>
 </head>
 <body>
 
